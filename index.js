48b541b05a402f191b5e833d4542afbbe98ade68
ASAD321-stack <creativelu321@gmail.com>
2021-11-24 14:34:04 -0800

    make it better

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
 mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology:true})
 .then(()=>console.log("connected"))
 .catch((err)=>{
